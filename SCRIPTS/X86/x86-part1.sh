b4946c1423d843475d833a7d0c3ab4ab4603a068
42 <sunsan05@Gmail.com>
2025-03-07 10:12:31 +0800

    Update x86-part1.sh

--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -10,5 +10,5 @@
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Add PassWall feeds
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
